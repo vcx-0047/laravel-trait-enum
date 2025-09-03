c56dbfca5c3c1af25690d69e791929b97648e8f0
vcx-0047 <167695472+vcx-0047@users.noreply.github.com>
2025-09-03 11:55:28 +0100

    Create usage.php

---
+++ usage.php
@@ -0,0 +1,3 @@
+Example::getEnum('culomn name '); //get enumes in the model 
+Example::getEnums(); //get all enumes in the model 
+Example::getEnums(['array of cullomns']); //get selected culomns enumes in the model 
