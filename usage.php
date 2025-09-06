0c556671731154315a368fea92d801d7bf420db8
vcx-0047 <167695472+vcx-0047@users.noreply.github.com>
2025-09-06 09:39:37 +0100

    Update usage.php

--- usage.php
+++ usage.php
@@ -1,8 +1,19 @@
-Contract::getEnum('status');
+Example::getEnum('status');
 // Example: ['pending', 'active', 'closed']
 
-Contract::getEnums();
+Example::getEnums();
 // [
 //   'status' => ['pending', 'active', 'closed'],
+//   'type'   => ['full_time', 'part_time']
+// ]
+
+Example::setEnum('status', 'off');
+// [
+//   'status' => ['pending', 'active', 'closed' ,'off'],
+//   'type'   => ['full_time', 'part_time']
+// ]
+Example::removeEnum('status', 'active');
+// [
+//   'status' => ['pending', 'closed' ,'off'],
 //   'type'   => ['full_time', 'part_time']
 // ]
